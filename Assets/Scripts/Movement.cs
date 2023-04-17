59e2caa220672258274fa499620a35eaf11f96b3
Miłosz Andrzej Piekarski <milosz.piekarski2003@gmail.com>
2023-04-17 22:19:25 +0200

    Nearly perfect touch input movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,11 +7,15 @@ public class Movement : MonoBehaviour
     Rigidbody2D playerrb;
     float movementSpeed = 5f;
     void OnEnable() {
-        TouchSimulation.Enable();    
+        EnhancedTouchSupport.Enable();  
     }
     void Start()
     {
         playerrb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        TouchInput();   
     }
 
     public void Move(InputAction.CallbackContext context){
@@ -19,22 +23,41 @@ public class Movement : MonoBehaviour
         playerrb.velocity = new Vector2(context.ReadValue<Vector2>().x * movementSpeed, 0);
     }
 
-    public void MoveTouch(InputAction.CallbackContext context)
-    {
-        Debug.Log(context.ReadValue<Vector2>().x);
-        Vector2 inputValue = context.ReadValue<Vector2>();
-        if(inputValue.x == 0)
-        {
+    public void TouchInput(){
+        var fingers = UnityEngine.InputSystem.EnhancedTouch.Touch.activeFingers;
+        var lastTouch = fingers[fingers.Count-1].lastTouch;
+        if(!lastTouch.isInProgress){
             playerrb.velocity = new Vector2(0, 0);
+            return;
         }
-        else if(inputValue.x < Screen.width/2)
-        {
-            playerrb.velocity = new Vector2(-1 * movementSpeed, 0);
-        }
-        else
-        {
-            playerrb.velocity = new Vector2(1 * movementSpeed, 0);
+        Vector2 position = lastTouch.screenPosition;
+        position -= new Vector2(Screen.width/2,0);
+        if (position.x > 0){
+            //If on the right side of screen, move toward positive x
+            playerrb.velocity = new Vector2(1f * movementSpeed, 0);
+        }else{
+            //Else move toward negative x
+            playerrb.velocity = new Vector2(-1f * movementSpeed, 0);
         }
     }
+
+    // public void MoveTouch(InputAction.CallbackContext context)
+    // {
+    //     Vector2 inputValue = context.ReadValue<Vector2>();
+    //     if (context.phase == InputActionPhase.Performed){
+    //         //If touch position is 0 (or not present), stop the player
+    //         playerrb.velocity = new Vector2(0, 0);
+    //         return;
+    //     }
+    //     //Check touch position
+    //     inputValue -= new Vector2(Screen.width/2,0);
+    //     if (inputValue.x > 0){
+    //         //If on the right side of screen, move toward positive x
+    //         playerrb.velocity = new Vector2(1f * movementSpeed, 0);
+    //     }else{
+    //         //Else move toward negative x
+    //         playerrb.velocity = new Vector2(-1f * movementSpeed, 0);
+    //     }
+    // }
     
 }
