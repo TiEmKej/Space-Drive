9409fb93236a5e335929139332b24cad7cc00f15
Miłosz Andrzej Piekarski <milosz.piekarski2003@gmail.com>
2023-04-18 23:59:56 +0200

    Some changes

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GameControllerScript : MonoBehaviour
 {
+    public Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-         Application.targetFrameRate = 60;
+        Application.targetFrameRate = 60;
+        Debug.Log(camera.rect);
+
     }
 
     // Update is called once per frame
