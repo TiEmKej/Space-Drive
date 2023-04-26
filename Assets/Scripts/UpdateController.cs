2ff7ddce6cfbe8dbf83d18a9c4fa6ed7431ce9d9
Miłosz Andrzej Piekarski <milosz.piekarski2003@gmail.com>
2023-04-26 19:15:47 +0200

    InApp Updates Handling

---
+++ Assets/Scripts/UpdateController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Google.Play.AppUpdate;
+using Google.Play.Common;
+
+public class UpdateController : MonoBehaviour
+{
+    AppUpdateManager appUpdateManager ;
+    void Start()
+    {
+        StartCoroutine(CheckForUpdate());
+    }
+
+    IEnumerator CheckForUpdate(){
+        PlayAsyncOperation<AppUpdateInfo, AppUpdateErrorCode> appUpdateInfoOperation =
+            appUpdateManager.GetAppUpdateInfo();
+
+        // Wait until the asynchronous operation completes.
+        yield return appUpdateInfoOperation;
+
+        if (appUpdateInfoOperation.IsSuccessful){
+            var appUpdateInfoResult = appUpdateInfoOperation.GetResult();
+            // Check AppUpdateInfo's UpdateAvailability, UpdatePriority,
+            // IsUpdateTypeAllowed(), etc. and decide whether to ask the user
+            // to start an in-app update.
+            var appUpdateOptions = AppUpdateOptions.ImmediateAppUpdateOptions();
+            StartCoroutine(StartImmediateUpdate(appUpdateInfoResult, appUpdateOptions));
+        }
+    }
+    IEnumerator StartImmediateUpdate(AppUpdateInfo appUpdateInfoResult, AppUpdateOptions appUpdateOptions){
+        // Creates an AppUpdateRequest that can be used to monitor the
+        // requested in-app update flow.
+        var startUpdateRequest = appUpdateManager.StartUpdate(
+            // The result returned by PlayAsyncOperation.GetResult().
+            appUpdateInfoResult,
+            // The AppUpdateOptions created defining the requested in-app update
+            // and its parameters.
+            appUpdateOptions);
+        yield return startUpdateRequest;
+    }
+}
